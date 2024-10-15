acbb73d8fbe4275c5e4cc1c5ca7392d7be405042
datungLA <97591324+datungLA@users.noreply.github.com>
2024-10-14 19:07:20 -0700

    Set up a navbar completed

--- Lab3/my-app/src/App.tsx
+++ Lab3/my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { ToDoList } from "./toDoList";
 import { Route, Routes } from "react-router-dom";
-import { StickyNotes } from "./stickyNote";
+import { StickyNotes } from "./stickyNotes";
+import { Navbar } from "./navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<StickyNotes />} />
         <Route path="/todolist" element={<ToDoList />} />
